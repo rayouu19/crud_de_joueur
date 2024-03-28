89d55e2e4bdb64f21022cc9b032aab80271865c3
rayouu19 <carpmer8737@gmail.com>
2024-03-28 07:55:02 +0100

    message du commit

--- build_mariadb/bdd.sql
+++ build_mariadb/bdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Mer 20 Mars 2024 à 10:22
--- Version du serveur :  5.7.11
--- Version de PHP :  7.0.3
+-- Client :  127.0.0.1
+-- Généré le :  Dim 24 Mars 2024 à 13:10
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,26 +14,33 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `login`
+-- Base de données :  `crudd`
 --
-CREATE DATABASE IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `login`;
-
+CREATE DATABASE IF NOT EXISTS `crudd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `crudd`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `livres`
+-- Structure de la table `joueurs`
 --
 
-CREATE TABLE `livres` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `auteur` varchar(255) NOT NULL,
-  `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `joueurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `poste` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=24 ;
+
+--
+-- Contenu de la table `joueurs`
+--
+
+INSERT INTO `joueurs` (`id`, `nom`, `prenom`, `poste`, `age`) VALUES
 
 -- --------------------------------------------------------
 
@@ -41,44 +48,13 @@ CREATE TABLE `livres` (
 -- Structure de la table `utilisateurs`
 --
 
-CREATE TABLE `utilisateurs` (
-  `pseudo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mdp` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` enum('user','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `pseudo` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`pseudo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`pseudo`, `mdp`, `role`) VALUES
-('rayou', '$2y$10$Z6xZZJYTSq1h/TnCCufNKOBnJxfhz7l5b0m6I4lYDzIaa3YdhkfyO', 'admin');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `livres`
---
-ALTER TABLE `livres`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`pseudo`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `livres`
---
-ALTER TABLE `livres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
